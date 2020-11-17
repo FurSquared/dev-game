9bcc2765e6ab1961955631b5e62e24e5cd7666b8
Phillip Pollard <phil@crescendo.net>
2020-11-16 23:32:34 -0500

    Adding a base redirect into the app.

--- f2game/urls.py
+++ f2game/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import include, path
 
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
     path('dashboard/', include('dashboard.urls')),
+    path('', lambda request: redirect('/dashboard/', permanent=True)),
 ]
