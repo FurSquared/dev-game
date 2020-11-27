08644b77a851f36667b006232a87ada1946c666b
Phillip Pollard <phil@crescendo.net>
2020-11-27 15:05:01 -0500

    Scratch model

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Tokens(models.Model):
+    token = models.CharField(max_length=25, primary_key=True)
+
+
+class TokensCollected(models.Model):
+    userid = models.IntegerField()
+    token = models.ForeignKey(Tokens, on_delete=models.DO_NOTHING)
+    collected_at = models.DateTimeField(default=timezone.now)
