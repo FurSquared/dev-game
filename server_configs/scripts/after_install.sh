3b28f05d69df3a644ae605d9289b2c62bbe8de8c
Kataze Skunk <kataze@fursplosion.com>
2020-11-29 02:09:37 +0000

    Move local_settings symlink earlier in the post install process

--- server_configs/scripts/after_install.sh
+++ server_configs/scripts/after_install.sh
@@ -2,6 +2,7 @@
 
 # Install libaries
 cd /var/www/backend
+sudo ln -sf /var/www/prod_resources/local_settings.py /var/www/backend/f2game
 virtualenv -p python3 venv
 source venv/bin/activate
 pip install -r requirements.txt
@@ -11,7 +12,6 @@ python manage.py collectstatic --no-input
 # Set permission for all files
 sudo chown -R www-data:www-data /var/www/
 
-sudo ln -sf /var/www/prod_resources/local_settings.py /var/www/backend/f2game
 sudo ln -sf /etc/nginx/sites-available/f2game.conf /etc/nginx/sites-enabled
 
 # Restart services
