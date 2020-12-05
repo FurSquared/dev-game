8468990bad876fa3b660f39088cf2c8b5a494062
Phillip Pollard <phil@crescendo.net>
2020-12-05 17:11:57 -0500

    Apply design to password reset templates.
    * Had to re-order INSTALLED_APPS in settings.py so our templates
      override default django templates.
    * Built out the password reset templates
    * Also: Cleaned up inheritance of title throughout the templates

--- f2game/urls.py
+++ f2game/urls.py
@@ -1,18 +1,5 @@
-"""f2game URL Configuration
+"""f2game URL Configuration"""
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.shortcuts import redirect
@@ -20,9 +7,9 @@ from django.urls import include, path
 from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path('', lambda request: redirect('/dashboard/', permanent=True)),
     path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
     path('dashboard/', include('dashboard.urls')),
-    path('', lambda request: redirect('/dashboard/', permanent=True)),
     path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('img/favicon.ico')))
 ]
