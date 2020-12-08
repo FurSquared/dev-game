dba555934631c53c50515f2a50c795964352f719
Kataze Skunk <kataze@fursplosion.com>
2020-12-08 03:25:56 +0000

    Add daily db backup

--- server_configs/scripts/before_install.sh
+++ server_configs/scripts/before_install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 sudo apt-get update
-sudo apt-get install -y python3 python3-pip nginx gunicorn
+sudo apt-get install -y python3 python3-pip nginx gunicorn awscli
 pip3 install pipenv
 sudo rm -rf /var/www/backend
