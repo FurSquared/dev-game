d66bcb72da08c3d300031cb966f2cecb7feb2dbc
Kataze Skunk <kataze@fursplosion.com>
2020-12-08 02:52:53 +0000

    Remove www-data sudo from pipenv

--- server_configs/scripts/after_install.sh
+++ server_configs/scripts/after_install.sh
@@ -6,8 +6,8 @@ export DJANGO_SETTINGS_MODULE=f2game.local_settings
 # Install libaries
 cd /var/www/backend
 sudo ln -sf /var/www/prod_resources/local_settings.py /var/www/backend/f2game
-sudo -u www-data pipenv install
-sudo -u www-data pipenv shell
+pipenv install
+pipenv shell
 python manage.py migrate
 python manage.py collectstatic --no-input
 
