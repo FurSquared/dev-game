d1c57b9fc4e7760afd16b2b0cf0c23fe74df5663
Draggor <draggor@gmail.com>
2020-12-17 02:34:03 -0600

    First pass at upload rewards, don't know if the many to many will work with get_or_create like this

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import index, EnterCodeView, UploadCodesView
+from .views import index, EnterCodeView, UploadCodesView, UploadRewardsView
 
 urlpatterns = [
     path('', index, name='index'),
     path('enter_code', EnterCodeView.as_view(), name='enter_code'),
     path('upload_codes', UploadCodesView.as_view(), name='upload_codes'),
+    path('upload_rewards', UploadRewardsView.as_view(), name='upload_rewards'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -44,7 +44,10 @@ class UploadCodesView(View):
         if not request.user.is_superuser:
             return HttpResponse('Unauthorized', status=401)
 
-        return render(request, 'upload_codes.html')
+        context = {
+            'title': 'Upload Codes',
+        }
+        return render(request, 'upload_csv.html', context)
 
 
     @method_decorator(login_required)
@@ -55,6 +58,7 @@ class UploadCodesView(View):
         decoded_file = request.FILES['csvfile'].read().decode('UTF-8').splitlines()
         reader = csv.DictReader(decoded_file)
 
+        keys = ['code', 'gm_note', 'reward_text', 'valid_from']
         new_codes = []
         updated_codes = []
         for row in reader:
@@ -68,15 +72,28 @@ class UploadCodesView(View):
                     'valid_from': valid_from,
                 }
             )
+
             if created:
-                new_codes.append(token)
+                new_codes.append(' | '.join([str(getattr(token, key)) for key in keys]))
             else:
-                updated_codes.append(token)
+                updated_codes.append(' | '.join([str(getattr(token, key)) for key in keys]))
+
+
         context = {
-            'new_codes': new_codes,
-            'updated_codes': updated_codes,
+            'title': 'Upload Codes',
+            'heading': ' | '.join(keys),
+            'sections': [
+                {
+                    'title': 'New Codes',
+                    'values': new_codes,
+                },
+                {
+                    'title': 'Updated Codes',
+                    'values': updated_codes,
+                },
+            ]
         }
-        return render(request, 'upload_codes.html', context)
+        return render(request, 'upload_csv.html', context)
 
 class UploadRewardsView(View):
 
@@ -85,11 +102,55 @@ class UploadRewardsView(View):
         if not request.user.is_superuser:
             return HttpResponse('Unauthorized', status=401)
 
-        return render(request, 'upload_rewards.html')
+        context = {
+            'title': 'Upload Rewards',
+        }
+        return render(request, 'upload_csv.html', context)
 
 
     @method_decorator(login_required)
     def post(self, request, *args, **kwargs):
         if not request.user.is_superuser:
             return HttpResponse('Unauthorized', status=401)
-        return HttpResponse(status=204)
+
+        decoded_file = request.FILES['csvfile'].read().decode('UTF-8').splitlines()
+        reader = csv.DictReader(decoded_file)
+
+        keys = ['name', 'gm_note count', 'required_tokens', 'reward_text', 'valid_from']
+        new_rewards = []
+        updated_rewards = []
+        for row in reader:
+            valid_from = None if len(row['valid_from']) == 0 else row['valid_from']
+            required_tokens = [item.strip() for item in row['required_tokens'].split(',')]
+
+            reward, created = Reward.objects.get_or_create(
+                name=row['name'],
+                defaults={
+                    'gm_note': row['gm_note'],
+                    'reward_text': row['reward_text'],
+                    'valid_from': valid_from,
+                    'count': row['count'],
+                    'required_tokens': required_tokens,
+                }
+            )
+
+            if created:
+                new_rewards.append(' | '.join([str(getattr(token, key)) for key in keys]))
+            else:
+                updated_rewards.append(' | '.join([str(getattr(token, key)) for key in keys]))
+
+        context = {
+            'title': 'Upload Rewards',
+            'heading': ' | '.join(keys),
+            'sections': [
+                {
+                    'title': 'New Rewards',
+                    'values': new_codes,
+                },
+                {
+                    'title': 'Updated Rewards',
+                    'values': updated_codes,
+                },
+            ]
+        }
+        return render(request, 'upload_csv.html', context)
