79b6646312822050f7a5ad194bafee4aa15ea063
1.0 <one-point-oh@con.glomerate.co>
2021-02-26 00:32:15 +0000

    Helping

---
+++ README.md
@@ -0,0 +1,6 @@
+# README
+2.0 isn't all that great. I'm still helpful!
+
+## I can be helpful too
+* `{0TEMET}`
+* `{0NOSCE}`
--- dashboard/tests/test_collected_token.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase
-
-from dashboard.models import Token, CollectedToken
-
-
-class CollectedTokenTests(TestCase):
-    pass
